a6880cea5205e39daa6d97d2e0c16113a7eb7632
KashyapHegdeKota <kota2022kashyap@gmail.com>
2025-09-14 18:42:38 -0700

    Made visualize_bicep_curl.py a function which is then called in main.py. Also fixed the issue of pyenv and venv folder getting deleted.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from movella_dot_manager import setup_movella_dots, record_quaternions_to_csv
+from visualize_bicep_curl import visualize_bicep_curl
 import time
 
 start_time = time.time()
@@ -10,4 +11,5 @@ if manager:
     print("Devices ready!")
     record_quaternions_to_csv(manager, f"{start_time}_output.csv", duration=10)
     # When done:
+    visualize_bicep_curl(f"{start_time}_output.csv")
     manager.cleanup()
